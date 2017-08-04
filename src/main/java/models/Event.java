3d323bc427150a6816628519ebea15c154edeec7
RxNunez <Nunez.ryan@gmail.com>
2017-08-04 09:43:30 -0700

    include initial data typs and objects for event project

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,4 +1,20 @@
 package models;
 
-public class Event {
+class Event {
+    private Integer custGuests;
+    private Integer custMeals;
+    private Integer custBeverages;
+    private Integer custEntertainment;
+    private Integer cost = 0;
+    private Integer discount;
+    private String mealoption = "";
+    private String beverageoption = "";
+    private String entertainmentoption = "";
+
+    public Event(Integer guests, Integer meals, Integer beverages, Integer entertainment) {
+        custGuests = guests;
+        custMeals = meals;
+        custBeverages = beverages;
+        custEntertainment = entertainment;
+    }
 }
